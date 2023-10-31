8e6dbd88b33ec4fbd706aa048015240762c92a64
Nhahan <kisy324@naver.com>
2023-10-31 23:06:09 +0900

    peek

--- webflux-demo/src/main/java/com/webflux/demo/service/MathService.java
+++ webflux-demo/src/main/java/com/webflux/demo/service/MathService.java
@@ -16,6 +16,8 @@ public class MathService {
 
     public List<Response> multiplicationTable(int input) {
         return IntStream.rangeClosed(1, 10)
+                .peek(i -> SleepUtil.SleepSeconds(1))
+                .peek(i -> System.out.println("math-service processing: " + i))
                 .mapToObj(i -> new Response(i * input))
                 .collect(Collectors.toList());
     }
