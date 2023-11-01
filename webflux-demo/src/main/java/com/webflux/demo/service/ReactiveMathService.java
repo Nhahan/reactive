1f8e9729019e88433d2e8bde9849eb28caa257ca
Nhahan <kisy324@naver.com>
2023-11-01 22:59:16 +0900

    example: ReactiveMathService.multiplicationTable()

--- webflux-demo/src/main/java/com/webflux/demo/service/ReactiveMathService.java
+++ webflux-demo/src/main/java/com/webflux/demo/service/ReactiveMathService.java
@@ -2,6 +2,7 @@ package com.webflux.demo.service;
 
 import com.webflux.demo.dto.Response;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -9,5 +10,12 @@ public class ReactiveMathService {
 
     public Mono<Response> findSquare(int input) {
         return Mono.fromSupplier(() -> new Response(input * input));
+    }
+
+    public Flux<Response> multiplicationTable(int input) {
+        return Flux.range(1, 10)
+                .delayElements(java.time.Duration.ofSeconds(1))
+                .doOnNext(i -> System.out.println("reactive math-service processing: " + i))
+                .map(i -> new Response(i * input));
     }
 }
