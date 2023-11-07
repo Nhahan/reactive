e6cb3f79691f5401d2941fa89befd83602440cf3
Nhahan <kisy324@naver.com>
2023-11-08 00:07:00 +0900

    example: InputValidationException RuntimeException

--- webflux-demo/src/main/java/com/webflux/demo/exception/InputValidationException.java
+++ webflux-demo/src/main/java/com/webflux/demo/exception/InputValidationException.java
@@ -4,4 +4,18 @@ public class InputValidationException extends RuntimeException{
 
     private static final String DEFAULT_MESSAGE = "allowed range is 10 - 20";
     private static final int code = 100;
+    private final int input;
+
+    public InputValidationException(int input) {
+        super(DEFAULT_MESSAGE);
+        this.input = input;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public int getInput() {
+        return input;
+    }
 }
