5fb0faacea314b4431cb7d663c01a250f69b92ab
Nhahan <kisy324@naver.com>
2023-11-14 23:04:50 +0900

    example: RouterConfig

---
+++ webflux-demo/src/main/java/com/webflux/demo/config/RouterConfig.java
@@ -0,0 +1,13 @@
+package com.webflux.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterConfig {
+
+//    @Bean
+//    public RouterFunction<ServerResponse> serverResponseRouterFunction() {
+//        return RouterFunctions.route()
+//                .GET("/router/square/{input}", )
+//    }
+}
