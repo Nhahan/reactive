6d65f3160589cec22b4518bffd34138fcfa3ec58
Nhahan <kisy324@naver.com>
2023-12-02 23:37:36 +0900

    add: /router/square/{input}

--- webflux-demo/src/main/java/com/webflux/demo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/webflux/demo/config/RouterConfig.java
@@ -1,13 +1,22 @@
 package com.webflux.demo.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
+@RequiredArgsConstructor
 public class RouterConfig {
 
-//    @Bean
-//    public RouterFunction<ServerResponse> serverResponseRouterFunction() {
-//        return RouterFunctions.route()
-//                .GET("/router/square/{input}", )
-//    }
+    private final RequestHandler requestHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> serverResponseRouterFunction() {
+        return RouterFunctions.route()
+                .GET("/router/square/{input}", requestHandler::squareHandler)
+                .build();
+    }
 }
