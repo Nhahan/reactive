766383595f39c086f26735c7a835f66709323fe6
Nhahan <kisy324@naver.com>
2023-12-06 00:29:35 +0900

    exceptionHandler / squareHandlerWithValidation router

--- webflux-demo/src/main/java/com/webflux/demo/config/RouterConfig.java
+++ webflux-demo/src/main/java/com/webflux/demo/config/RouterConfig.java
@@ -1,11 +1,17 @@
 package com.webflux.demo.config;
 
+import com.webflux.demo.dto.InputFailedValidationResponse;
+import com.webflux.demo.exception.InputValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.util.function.BiFunction;
 
 @Configuration
 @RequiredArgsConstructor
@@ -20,6 +26,19 @@ public class RouterConfig {
                 .GET("/router/table/{input}", requestHandler::tableHandler)
                 .GET("/router/table/{input}/stream", requestHandler::tableStreamHandler)
                 .POST("/router/multiply", requestHandler::multiplyHandler)
+                .GET("router/square/{input}/validation", requestHandler::squareHandlerWithValidation)
+                .onError(InputValidationException.class, exceptionHandler())
                 .build();
+    }
+
+    private BiFunction<Throwable, ServerRequest, Mono<ServerResponse>> exceptionHandler() {
+        return (err, req) -> {
+            InputValidationException ex = (InputValidationException) err;
+            InputFailedValidationResponse response = new InputFailedValidationResponse();
+            response.setInput(ex.getInput());
+            response.setMessage(ex.getMessage());
+            response.setCode(ex.getCode());
+            return ServerResponse.badRequest().bodyValue(response);
+        };
     }
 }
