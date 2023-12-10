11a16f2bad8f5ca7fc67ffd341cdd98ffe735348
Nhahan <kisy324@naver.com>
2023-12-10 22:43:03 +0900

    test: GetSignleResponse

--- webflux-demo/src/test/java/com/webflux/demo/GetSingleResponseTest.java
+++ webflux-demo/src/test/java/com/webflux/demo/GetSingleResponseTest.java
@@ -1,11 +1,24 @@
 package com.webflux.demo;
 
+import com.webflux.demo.dto.Response;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
 
-public class GetSingleResponseTest {
+public class GetSingleResponseTest extends BaseTest {
 
     @Autowired
     private WebClient webClient;
+
+    @Test
+    public void blockTest() {
+        Response response = webClient.get()
+                .uri("router/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class) // Mono<Response>
+                .block();
+
+        System.out.println("response: " + response);
+    }
 
 }
