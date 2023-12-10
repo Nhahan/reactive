7ad229a32f7eff231ceb98459c83d1d473f01aa9
Nhahan <kisy324@naver.com>
2023-12-10 19:03:56 +0900

    subtration handler

--- webflux-demo/src/main/java/com/webflux/demo/config/CalculatorHandler.java
+++ webflux-demo/src/main/java/com/webflux/demo/config/CalculatorHandler.java
@@ -5,14 +5,25 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.function.BiFunction;
+
 @Service
 public class CalculatorHandler {
 
     // calculator/{a}/{b}
     public Mono<ServerResponse> additionHandler(ServerRequest request) {
+        return process(request, (a, b) -> ServerResponse.ok().bodyValue(a + b));
+    }
+
+    public Mono<ServerResponse> subtractionHandler(ServerRequest request) {
+        return process(request, (a, b) -> ServerResponse.ok().bodyValue(a - b));
+    }
+
+    private Mono<ServerResponse> process(ServerRequest request,
+                                         BiFunction<Integer, Integer, Mono<ServerResponse>> operation) {
         int a = getValue(request, "a");
         int b = getValue(request, "b");
-        return ServerResponse.ok().bodyValue(a + b);
+        return operation.apply(a, b);
     }
 
     private int getValue(ServerRequest request, String key) {
