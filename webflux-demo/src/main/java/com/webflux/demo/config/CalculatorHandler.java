77e60f8e699a13fba228d2909ac0e96c768ec862
Nhahan <kisy324@naver.com>
2023-12-10 17:48:50 +0900

    additionHandler

--- webflux-demo/src/main/java/com/webflux/demo/config/CalculatorHandler.java
+++ webflux-demo/src/main/java/com/webflux/demo/config/CalculatorHandler.java
@@ -1,4 +1,21 @@
 package com.webflux.demo.config;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Service
 public class CalculatorHandler {
+
+    public Mono<ServerResponse> additionHandler(ServerRequest request) {
+        int a = getValue(request, "a");
+        int b = getValue(request, "b");
+        return ServerResponse.ok().bodyValue(a + b);
+    }
+
+    private int getValue(ServerRequest request, String key) {
+        return Integer.parseInt(request.pathVariable(key));
+    }
+
 }
