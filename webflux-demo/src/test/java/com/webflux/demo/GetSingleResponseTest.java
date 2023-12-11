3fd6c2978bd94101ccf789679e280579c1056f62
Nhahan <kisy324@naver.com>
2023-12-11 23:46:31 +0900

    stepVerifierTest init

--- webflux-demo/src/test/java/com/webflux/demo/GetSingleResponseTest.java
+++ webflux-demo/src/test/java/com/webflux/demo/GetSingleResponseTest.java
@@ -4,6 +4,7 @@ import com.webflux.demo.dto.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 public class GetSingleResponseTest extends BaseTest {
 
@@ -20,6 +21,15 @@ public class GetSingleResponseTest extends BaseTest {
                 .block();
 
         System.out.println("response: " + response);
+    }
+
+    @Test
+    public void stepVerifierTest() {
+        Mono<Response> responseMono = webClient
+                .get()
+                .uri("router/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);// Mono<Response>
     }
 
 }
